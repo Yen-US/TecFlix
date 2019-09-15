77d7aa9d0a65b9b16861e536854730d31d1e87d0
yenus <yen21000@gmail.com>
2019-09-15 14:26:55 -0600

    Se implementa una interfaz simple

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "Clases/pelicula.cpp"
 #include "Archivo_csv/lectura.cpp"
+#include "Grafica/pagina.cpp"
+
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
     //Lectura del .csv
-    List listaP;
     lectura l;
-    listaP = l.lect();
+    List L=l.lect();
+    List *ptoList = (List *)malloc(sizeof(L));
+    *ptoList=L;
+
+    QApplication app(argc, argv);
+    Ventana ventana;
+    ventana.resize(800, 300);
+    ventana.setWindowTitle("TecFlix");
+    ventana.show();
+
+    return app.exec();
 }
