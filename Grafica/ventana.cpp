624729ae3238d11b41aef9bb4ac5c72a15454f87
yenus <yen21000@gmail.com>
2019-09-18 21:22:01 -0600

    Se sustituye el scroll por la interfaz por una navegacion con flechas

--- Grafica/ventana.cpp
+++ Grafica/ventana.cpp
@@ -3,6 +3,7 @@
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets>
+#include <QKeyEvent>
 #include "../DesdeIMdB/leerIMdB.h"
 #include "../Lista/list.h"
 #include "../DesdeIMdB/descarga.cpp"
@@ -15,7 +16,7 @@ public:
     Ventana(QWidget *parent = 0);
 
 protected:
-    virtual void wheelEvent(QWheelEvent *event);
+    void keyPressEvent(QKeyEvent *event) override;
 
 private:
     QPushButton* btn1;
@@ -36,6 +37,9 @@ private:
     HTTPDownloader http;
 
     string ruta="/home/yenus/CLionProjects/TecFlix/pst.jpg";
+
+    bool eventFilter(QObject *object, QEvent *event);
+
 };
 
 Ventana::Ventana(QWidget *parent) : QWidget(parent)
@@ -43,152 +47,167 @@ Ventana::Ventana(QWidget *parent) : QWidget(parent)
     const int dist =10;
     const int x= 182;
     const int y= 268;
+    QApplication::instance()->installEventFilter(this);
+
     btn1 = new QPushButton("", this);
+    btn1->installEventFilter(this);
     btn1->setGeometry(dist, dist+50, x , y);
     connect(btn1, &QPushButton::clicked, this, &QWidget::close);
 
     btn2 = new QPushButton("", this);
+    btn2->installEventFilter(this);
     btn2->setGeometry((2*dist+x), dist+50, x, y);
     connect(btn2, &QPushButton::clicked, this, &QWidget::close);
 
     btn3 = new QPushButton("", this);
+    btn3->installEventFilter(this);
     btn3->setGeometry((3*dist+2*x), dist+50, x, y);
     connect(btn3, &QPushButton::clicked, this, &QWidget::close);
 
     btn4 = new QPushButton("", this);
+    btn4->installEventFilter(this);
     btn4->setGeometry(4*dist+3*x, dist+50, x, y);
     connect(btn4, &QPushButton::clicked, this, &QWidget::close);
 
     btn5 = new QPushButton("", this);
+    btn5->installEventFilter(this);
     btn5->setGeometry(dist, dist*2+y+50, x , y);
     connect(btn5, &QPushButton::clicked, this, &QWidget::close);
 
     btn6 = new QPushButton("", this);
+    btn6->installEventFilter(this);
     btn6->setGeometry((2*dist+x), dist*2+y+50, x, y);
     connect(btn6, &QPushButton::clicked, this, &QWidget::close);
 
     btn7 = new QPushButton("", this);
+    btn7->installEventFilter(this);
     btn7->setGeometry((3*dist+2*x), dist*2+y+50, x, y);
     connect(btn7, &QPushButton::clicked, this, &QWidget::close);
 
     btn8 = new QPushButton("", this);
+    btn8->installEventFilter(this);
     btn8->setGeometry(4*dist+3*x, dist*2+y+50, x, y);
     connect(btn8, &QPushButton::clicked, this, &QWidget::close);
 
     btn9 = new QPushButton("", this);
+    btn9->installEventFilter(this);
     btn9->setGeometry(dist, dist*3+y*2+50, x , y);
     connect(btn9, &QPushButton::clicked, this, &QWidget::close);
 
     btn10 = new QPushButton("", this);
+    btn10->installEventFilter(this);
     btn10->setGeometry((2*dist+x), dist*3+y*2+50, x, y);
     connect(btn10, &QPushButton::clicked, this, &QWidget::close);
 
     btn11 = new QPushButton("", this);
+    btn11->installEventFilter(this);
     btn11->setGeometry((3*dist+2*x), dist*3+y*2+50, x, y);
     connect(btn11, &QPushButton::clicked, this, &QWidget::close);
 
     btn12 = new QPushButton("", this);
+    btn12->installEventFilter(this);
     btn12->setGeometry(4*dist+3*x, dist*3+y*2+50, x, y);
     connect(btn12, &QPushButton::clicked, this, &QWidget::close);
-
 
     /*QLabel* lbl = new QLabel(this);
     lbl->setGeometry(130, 10, 800, 30);
     lbl->setFont(QFont("Arial", 12));
     lbl->setText("Hello World");*/
 }
+bool Ventana::eventFilter(QObject *object, QEvent *event) {
+    if (event->type() == QEvent::KeyPress) {
+        List L = l.lect();
+        int cantPel = L.cant();
+        Pagina p;
+        bool ejecutando = false;
+        string ruta;
+        QKeyEvent *key_event = static_cast<QKeyEvent *>(event);
+        if (key_event->key() == Qt::Key_Down) {
+            if (pActual == 0) {}
+            else {
+                List pagActL = p.newpag(pActual, cantPel, L);
+                for (int i = 0; i < p.getcantPag(); i++) {
+                    string data = http.download(pagActL.obt_by_position(i).getImdb());
 
-void Ventana::wheelEvent(QWheelEvent *event) {
-    List L=l.lect();
-    int cantPel=L.cant();
-    Pagina p;
-    bool ejecutando=false;
-    string ruta;
+                    j.download_jpeg(http.getImageURL(data), i);
 
-    if (event->delta()){
-        ejecutando = true;
-    }
-    else{}
-    if (ejecutando){
-    if (event->delta()<0) {
-        if(pActual==0){}
-        else{
-            List pagActL= p.newpag(pActual,cantPel,L);
-            for(int i=0;i<p.getcantPag();i++){
-                string data=http.download(pagActL.obt_by_position(i).getImdb());
+                    string str = to_string(i);
 
-                j.download_jpeg(http.getImageURL(data),i);
+                    ruta = "/home/yenus/CLionProjects/TecFlix/pst" + str + ".jpg";
 
-                string str = to_string(i);
+                    QPixmap pix1(ruta.c_str());
+                    QPixmap pix = pix1.scaled(182, 268, Qt::IgnoreAspectRatio);
 
-                ruta="/home/yenus/CLionProjects/TecFlix/pst"+str+".jpg";
+                    QIcon ButtonIcon(pix);
+                    switch (i + 1) {
+                        case 1:
+                            btn1->setIcon(ButtonIcon);
+                            btn1->setIconSize(btn1->rect().size());
+                            break;
+                        case 2:
+                            btn2->setIcon(ButtonIcon);
+                            btn2->setIconSize(btn1->rect().size());
+                            break;
+                        case 3:
+                            btn3->setIcon(ButtonIcon);
+                            btn3->setIconSize(btn1->rect().size());
+                            break;
+                        case 4:
+                            btn4->setIcon(ButtonIcon);
+                            btn4->setIconSize(btn1->rect().size());
+                            break;
+                        case 5:
+                            btn5->setIcon(ButtonIcon);
+                            btn5->setIconSize(pix.rect().size());
+                            break;
+                        case 6:
+                            btn6->setIcon(ButtonIcon);
+                            btn6->setIconSize(pix.rect().size());
+                            break;
+                        case 7:
+                            btn7->setIcon(ButtonIcon);
+                            btn7->setIconSize(pix.rect().size());
+                            break;
+                        case 8:
+                            btn8->setIcon(ButtonIcon);
+                            btn8->setIconSize(pix.rect().size());
+                            break;
+                        case 9:
+                            btn9->setIcon(ButtonIcon);
+                            btn9->setIconSize(pix.rect().size());
+                            break;
+                        case 10:
+                            btn10->setIcon(ButtonIcon);
+                            btn10->setIconSize(pix.rect().size());
+                            break;
+                        case 11:
+                            btn11->setIcon(ButtonIcon);
+                            btn11->setIconSize(pix.rect().size());
+                            break;
+                        case 12:
+                            btn12->setIcon(ButtonIcon);
+                            btn12->setIconSize(pix.rect().size());
+                            break;
+                        default:
+                            cout << "error" << endl;
+                            break;
+                    }
 
-                QPixmap pix1(ruta.c_str());
-                QPixmap pix=pix1.scaled(182,268,Qt::IgnoreAspectRatio);
+                    remove(ruta.c_str());
 
-                QIcon ButtonIcon(pix);
-                switch (i+1){
-                    case 1:
-                        btn1->setIcon(ButtonIcon);
-                        btn1->setIconSize(btn1->rect().size());
-                        break;
-                    case 2:
-                        btn2->setIcon(ButtonIcon);
-                        btn2->setIconSize(btn1->rect().size());
-                        break;
-                    case 3:
-                        btn3->setIcon(ButtonIcon);
-                        btn3->setIconSize(btn1->rect().size());
-                        break;
-                    case 4:
-                        btn4->setIcon(ButtonIcon);
-                        btn4->setIconSize(btn1->rect().size());
-                        break;
-                    case 5:
-                        btn5->setIcon(ButtonIcon);
-                        btn5->setIconSize(pix.rect().size());
-                        break;
-                    case 6:
-                        btn6->setIcon(ButtonIcon);
-                        btn6->setIconSize(pix.rect().size());
-                        break;
-                    case 7:
-                        btn7->setIcon(ButtonIcon);
-                        btn7->setIconSize(pix.rect().size());
-                        break;
-                    case 8:
-                        btn8->setIcon(ButtonIcon);
-                        btn8->setIconSize(pix.rect().size());
-                        break;
-                    case 9:
-                        btn9->setIcon(ButtonIcon);
-                        btn9->setIconSize(pix.rect().size());
-                        break;
-                    case 10:
-                        btn10->setIcon(ButtonIcon);
-                        btn10->setIconSize(pix.rect().size());
-                        break;
-                    case 11:
-                        btn11->setIcon(ButtonIcon);
-                        btn11->setIconSize(pix.rect().size());
-                        break;
-                    case 12:
-                        btn12->setIcon(ButtonIcon);
-                        btn12->setIconSize(pix.rect().size());
-                        break;
-                    default:
-                        cout<<"error"<<endl;
-                        break;
                 }
-
-                remove(ruta.c_str());
 
             }
 
-        }
-    }
-    }else{
-        ejecutando=false;
-    }
+        }else if (key_event->key() == Qt::Key_Up){
 
+        }
+    } else {
+        // standard event processing
+        return QObject::eventFilter(object, event);
+    }
 }
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -10,25 +10,6 @@ int main(int argc, char *argv[]) {
     ventana.resize(778, 894);
     ventana.setWindowTitle("TecFlix");
     ventana.show();
-/*
-    lectura l;
-    List L=l.lect();
-    int cantPel=L.cant();
-    L.print();
-    Pelicula peli1=L.obt_by_position(5000);
-    cout<<"\n"+peli1.getImdb()<<endl;
-    Pagina p;
-    p.newpag(3,cantPel, L).print();*/
-
-
-
-    /*HTTPDownloader downloader;
-    string content = downloader.download("http://www.imdb.com/title/tt0814255/?ref_=fn_tt_tt_1");
-    //cout << content << endl;
-    //cout<< "\n"+downloader.getImageURL(content)<<endl;
-    jpeg j;
-    j.download_jpeg("https://m.media-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg",1);
-    cout<< "\n"+downloader.getTrailerURL(content)<<endl;*/
 
     return app.exec();
 }
